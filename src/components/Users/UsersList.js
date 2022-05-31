749b0b59b63c19d2d9b90e8df8a237eeb9dc617d
Vipin Kumar <vipinthakur3881@gmail.com>
2022-05-31 17:59:14 +0530

    initial commit

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '../UI/Card';
+const UserList =(props)=>{
+    return (
+    <Card className="{classes.users}">
+    <ul>
+        {props.users.map((user)=>(
+        <li key={user.id}>
+           {user.id} {user.name} ({user.age} years old)
+        </li>
+        ))}
+    </ul>
+    </Card>)
+};
+
+export default UserList;
